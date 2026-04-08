aa9f960ad1e6425e420eb9b643bf93b2589c1cc4
Mauricio Mityo Hidani <mauricio.hidani.work@gmail.com>
2026-04-08 19:55:31 -0300

    feat: ex00 subject test

--- ex00/main.cpp
+++ ex00/main.cpp
@@ -12,24 +12,47 @@ void printStatus(std::ostream& out, const std::string& target,
 }
 
 int main(void) {
-	int	a = 22;
-	int b = 42;
-
-	std::cout << "swap test" << std::endl;
-	printStatus(std::cout, "before", a, b);
-	::swap<int>(a, b);
-	printStatus(std::cout, "after", a, b);
-	std::cout << std::endl;
-
-	printStatus(std::cout, "min test", a, b);
-	int min = ::min(a, b);
-	std::cout << "min: " << min << std::endl;
-	std::cout << std::endl;
+	{
+		std::cout << "TEST 1" << std::endl;
+		int	a = 2;
+		int	b = 3;
 	
-	printStatus(std::cout, "max test", a, b);
-	int max = ::max(a, b);
-	std::cout << "max: " << max << std::endl;
-	std::cout << std::endl;
+		::swap( a, b );
+		std::cout << "a = " << a << ", b = " << b << std::endl;
+		std::cout << "min( a, b ) = " << ::min( a, b ) << std::endl;
+		std::cout << "max( a, b ) = " << ::max( a, b ) << std::endl;
+	
+		std::string c = "chaine1";
+		std::string d = "chaine2";
+
+		::swap(c, d);
+		std::cout << "c = " << c << ", d = " << d << std::endl;
+		std::cout << "min( c, d ) = " << ::min( c, d ) << std::endl;
+		std::cout << "max( c, d ) = " << ::max( c, d ) << std::endl;
+		std::cout << std::endl;
+	}
+
+	{
+		std::cout << "TEST 2" << std::endl;
+		int	a = 22;
+		int b = 42;
+
+		std::cout << "swap test" << std::endl;
+		printStatus(std::cout, "before", a, b);
+		::swap<int>(a, b);
+		printStatus(std::cout, "after", a, b);
+		std::cout << std::endl;
+
+		printStatus(std::cout, "min test", a, b);
+		int min = ::min(a, b);
+		std::cout << "min: " << min << std::endl;
+		std::cout << std::endl;
+		
+		printStatus(std::cout, "max test", a, b);
+		int max = ::max(a, b);
+		std::cout << "max: " << max << std::endl;
+		std::cout << std::endl;
+	}
 
 	return 0;
 }
